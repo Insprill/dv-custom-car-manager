ab6bd012bd0f12053a93fb8ce32768095ccbd6a0
Pierce Thompson <insprill@gmail.com>
2022-06-18 01:06:20 -0400

    Add ErrorDialog constructor for exceptions

--- src/main/java/net/insprill/customcarmanager/ui/dialog/ErrorDialog.java
+++ src/main/java/net/insprill/customcarmanager/ui/dialog/ErrorDialog.java
@@ -1,25 +1,46 @@
 package net.insprill.customcarmanager.ui.dialog;
 
+import javafx.event.ActionEvent;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
-import javafx.stage.Stage;
+import javafx.scene.control.TextArea;
 import javafx.stage.StageStyle;
 import net.insprill.customcarmanager.config.Locale;
+import net.insprill.customcarmanager.util.TextHelper;
 
 public class ErrorDialog extends Alert {
 
-    public ErrorDialog(Stage primaryStage, String errorMessage) {
+    public ErrorDialog(String errorMessage) {
         super(AlertType.ERROR, null, ButtonType.CLOSE);
-        init(primaryStage);
+        init();
         setContentText(errorMessage);
         showAndWait();
     }
 
-    private void init(Stage primaryStage) {
+    public ErrorDialog(Exception ex) {
+        super(AlertType.ERROR, null, ButtonType.CLOSE);
+        init();
+
+        String errorMessage = TextHelper.getStacktrace(ex);
+
+        ButtonType copyToClipboard = new ButtonType(Locale.getLine("dialog.error.copy-stacktrace-to-clipboard"), ButtonBar.ButtonData.BACK_PREVIOUS);
+        getDialogPane().getButtonTypes().add(0, copyToClipboard);
+
+        TextArea errorText = new TextArea(errorMessage);
+        errorText.setEditable(false);
+
+        getDialogPane().lookupButton(copyToClipboard).addEventFilter(ActionEvent.ACTION, e -> {
+            TextHelper.copyToClipboard(errorMessage);
+            e.consume();
+        });
+
+        showAndWait();
+    }
+
+    private void init() {
         initStyle(StageStyle.UTILITY);
-        getDialogPane().getStyleClass().add("error-dialog-pane");
         headerTextProperty().setValue(Locale.getLine("dialog.error.title"));
-        getDialogPane().getStylesheets().addAll(primaryStage.getScene().getStylesheets());
     }
 
 }
