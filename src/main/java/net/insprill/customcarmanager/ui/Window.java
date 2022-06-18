f3a6e915d2f20d7e4bf6565604b72d0d286cbc27
Pierce Thompson <insprill@gmail.com>
2022-06-18 01:05:14 -0400

    Make window class a singleton

--- src/main/java/net/insprill/customcarmanager/ui/Window.java
+++ src/main/java/net/insprill/customcarmanager/ui/Window.java
@@ -9,10 +9,28 @@ import net.insprill.customcarmanager.config.Locale;
 
 import java.io.IOException;
 
-public class Window extends Application {
+public final class Window extends Application {
+
+    // region Singleton
+    private static Window instance;
+
+    public static Window getInstance() {
+        return instance;
+    }
+
+    private static void setInstance(Window instance) {
+        Window.instance = instance;
+    }
+    // endregion
+
+    private Stage primaryStage;
 
     @Override
     public void start(Stage primaryStage) throws IOException {
+        Window.setInstance(this);
+
+        this.primaryStage = primaryStage;
+
         primaryStage.setTitle(Locale.getLine("window.title"));
 
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("ui/home.fxml"));
@@ -24,6 +42,10 @@ public class Window extends Application {
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
+    }
+
+    public Stage getPrimaryStage() {
+        return this.primaryStage;
     }
 
 }
