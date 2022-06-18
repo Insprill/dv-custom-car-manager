9526d5083e6176d36a8f02b5373c74c87bceb48f
Pierce Thompson <insprill@gmail.com>
2022-06-18 02:35:59 -0400

    Add support for installing cars from folders

--- src/main/java/net/insprill/customcarmanager/cars/CarManager.java
+++ src/main/java/net/insprill/customcarmanager/cars/CarManager.java
@@ -1,9 +1,14 @@
 package net.insprill.customcarmanager.cars;
 
 import net.insprill.customcarmanager.config.Config;
+import net.insprill.customcarmanager.config.Locale;
+import net.insprill.customcarmanager.ui.dialog.ErrorDialog;
+import net.insprill.customcarmanager.util.IO;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CarManager {
@@ -39,11 +44,62 @@ public class CarManager {
     }
 
     public void installCarFromFolder(File file) {
+        if (!file.isDirectory())
+            throw new IllegalArgumentException("File must be a directory");
+        file = findBaseFolder(file);
+        if (file == null)
+            return;
 
+        File installDir = new File(getCarsDir(), file.getName());
+
+        if (installDir.exists()) {
+            File config = findConfig(installDir);
+            if (config == null) {
+                new ErrorDialog(Locale.getLine("dialog.error.dir-already-exists"));
+                return;
+            }
+            new Car(installDir).delete();
+        }
+
+        try {
+            IO.copyDirectory(file, installDir);
+        } catch (IOException e) {
+            new ErrorDialog(e);
+            try {
+                IO.deleteDirectory(installDir);
+            } catch (IOException ex) {
+                new ErrorDialog(e);
+            }
+        }
     }
 
     public void installCarFromArchive(File file) {
 
+    }
+
+    private File findBaseFolder(File file) {
+        if (!file.isDirectory())
+            throw new IllegalArgumentException("File must be a directory");
+
+        File[] files = file.listFiles();
+
+        if (findConfig(file) != null)
+            return file;
+
+        for (File subFile : files) {
+            if (subFile.isDirectory()) {
+                File baseFolder = findBaseFolder(subFile);
+                if (baseFolder != null)
+                    return baseFolder;
+            }
+        }
+
+        new ErrorDialog(Locale.getLine("dialog.error.car-not-found"));
+        return null;
+    }
+
+    private File findConfig(File dir) {
+        return Arrays.stream(dir.listFiles()).filter(f -> f.getName().equals(CAR_CONFIG)).findFirst().orElse(null);
     }
 
 }
