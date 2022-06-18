83c110480844c61153762d1e0b36f4915204d83e
Pierce Thompson <insprill@gmail.com>
2022-06-18 01:43:54 -0400

    Move UI methods to the UIController

---
+++ src/main/java/net/insprill/customcarmanager/ui/UIController.java
@@ -0,0 +1,42 @@
+package net.insprill.customcarmanager.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import net.insprill.customcarmanager.config.Config;
+import net.insprill.customcarmanager.config.Locale;
+import net.insprill.customcarmanager.ui.factory.FileChooserFactory;
+import net.insprill.customcarmanager.ui.factory.FolderChooserFactory;
+
+import java.io.File;
+
+public class UIController {
+
+    @FXML
+    private void selectInstallDirectory() {
+        File file = FolderChooserFactory.newDialog(Locale.getLine("folder-chooser.dv-install-directory.title"));
+        if (file == null)
+            return;
+        String path = file.getAbsolutePath();
+        Config.setString("install-directory", path);
+        TextField lookup = (TextField) Window.getInstance().getPrimaryStage().getScene().lookup("#install_dir_field");
+        lookup.setText(path);
+    }
+
+    @FXML
+    private void installCarFromFolder() {
+        File file = FolderChooserFactory.newDialog(Locale.getLine("folder-chooser.install-car.title"));
+        if (file == null)
+            return;
+        Window.getInstance().getCarManager().installCarFromFolder(file);
+    }
+
+    @FXML
+    private void installCarFromArchive() {
+        File file = FileChooserFactory.newDialog(Locale.getLine("folder-chooser.install-car.title"), new FileChooser.ExtensionFilter("Archive", "*.zip"));
+        if (file == null)
+            return;
+        Window.getInstance().getCarManager().installCarFromArchive(file);
+    }
+
+}
--- src/main/java/net/insprill/customcarmanager/ui/Window.java
+++ src/main/java/net/insprill/customcarmanager/ui/Window.java
@@ -1,20 +1,13 @@
 package net.insprill.customcarmanager.ui;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TextField;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import net.insprill.customcarmanager.cars.CarManager;
-import net.insprill.customcarmanager.config.Config;
 import net.insprill.customcarmanager.config.Locale;
-import net.insprill.customcarmanager.ui.factory.FileChooserFactory;
-import net.insprill.customcarmanager.ui.factory.FolderChooserFactory;
 
-import java.io.File;
 import java.io.IOException;
 
 public final class Window extends Application {
@@ -59,36 +52,5 @@ public final class Window extends Application {
     public CarManager getCarManager() {
         return this.carManager;
     }
-
-    // region Actions
-
-    @FXML
-    private void selectInstallDirectory() {
-        File file = FolderChooserFactory.newDialog(Locale.getLine("folder-chooser.dv-install-directory.title"));
-        if (file == null)
-            return;
-        String path = file.getAbsolutePath();
-        Config.setString("install-directory", path);
-        TextField lookup = (TextField) getPrimaryStage().getScene().lookup("#install_dir_field");
-        lookup.setText(path);
-    }
-
-    @FXML
-    private void installCarFromFolder() {
-        File file = FolderChooserFactory.newDialog(Locale.getLine("folder-chooser.install-car.title"));
-        if (file == null)
-            return;
-        getCarManager().installCarFromFolder(file);
-    }
-
-    @FXML
-    private void installCarFromArchive() {
-        File file = FileChooserFactory.newDialog(Locale.getLine("folder-chooser.install-car.title"), new FileChooser.ExtensionFilter("Archive", "*.zip"));
-        if (file == null)
-            return;
-        getCarManager().installCarFromArchive(file);
-    }
-
-    // endregion
 
 }
