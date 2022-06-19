1104df70639e92dc5044a4629793bcc7cc21884d
Pierce Thompson <insprill@gmail.com>
2022-06-19 02:41:48 -0400

    Rename Config's config to json

--- src/main/java/net/insprill/customcarmanager/config/Config.java
+++ src/main/java/net/insprill/customcarmanager/config/Config.java
@@ -14,18 +14,18 @@ public class Config {
     private static File configFolder;
     private static File configFile;
 
-    private static JsonObject config;
+    private static JsonObject json;
 
     public static void init() throws IOException {
         configFolder = new File(System.getProperty("user.home") + File.separator + ".customcarmanager");
-        config = new JsonObject();
+        json = new JsonObject();
 
         configFile = new File(configFolder, "config.json");
 
         try (InputStream is = Config.class.getClassLoader().getResourceAsStream("config.json")) {
             if (!configFile.exists()) {
                 configFolder.mkdirs();
-                config = JsonParser.parseString(new String(is.readAllBytes())).getAsJsonObject();
+                json = JsonParser.parseString(new String(is.readAllBytes())).getAsJsonObject();
                 save();
             } else {
                 loadFromDisk();
@@ -35,23 +35,23 @@ public class Config {
 
     private static void loadFromDisk() throws IOException {
         try (InputStream is = Files.newInputStream(configFile.toPath(), StandardOpenOption.READ)) {
-            config = JsonParser.parseString(new String(is.readAllBytes())).getAsJsonObject();
+            json = JsonParser.parseString(new String(is.readAllBytes())).getAsJsonObject();
         }
     }
 
     public static String getString(String key) {
-        if (config.has(key)) {
-            return config.get(key).getAsString();
+        if (json.has(key)) {
+            return json.get(key).getAsString();
         }
         return null;
     }
 
     public static void setString(String key, String value) {
-        config.addProperty(key, value);
+        json.addProperty(key, value);
     }
 
     public static void save() throws IOException {
-        Files.writeString(configFile.toPath(), config.toString(), StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING);
+        Files.writeString(configFile.toPath(), json.toString(), StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING);
     }
 
     private Config() {
