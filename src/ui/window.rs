0a6994badf47c1d2435ece8c2e46a3554c7b9d55
Pierce Thompson <insprill@gmail.com>
2022-07-23 18:50:24 -0400

    Center window on screen when opening

--- src/ui/window.rs
+++ src/ui/window.rs
@@ -6,6 +6,7 @@ pub fn init() {
     let app = app::App::default();
     let theme = ColorTheme::new(color_themes::DARK_THEME);
     let mut ui = home::UserInterface::make_window();
+    ui.window.center_screen();
     ui.select_install_dir_button.set_callback(move |_| {
         println!("Works!");
     });
