e4f41bde465dfe2f511cb6ae04de11fa9ac197d0
Pierce Thompson <insprill@gmail.com>
2022-07-23 20:57:44 -0400

    Make UI look somewhat presentable

--- src/ui/window.rs
+++ src/ui/window.rs
@@ -1,5 +1,5 @@
 use fltk::{*, prelude::*};
-use fltk_theme::{color_themes, ColorTheme};
+use fltk_theme::{color_themes, ColorTheme, ThemeType, WidgetTheme};
 
 use crate::ui::home;
 use crate::util::utils;
@@ -7,6 +7,7 @@ use crate::util::utils;
 pub fn init() {
     let app = app::App::default();
     let theme = ColorTheme::new(color_themes::DARK_THEME);
+    let widget_theme = WidgetTheme::new(ThemeType::Dark);
     let mut ui = home::UserInterface::make_window();
     ui.window.center_screen();
     ui.select_install_dir_button.set_callback(move |_| {
@@ -15,5 +16,6 @@ pub fn init() {
         println!("{path}");
     });
     theme.apply();
+    widget_theme.apply();
     app.run().unwrap();
 }
