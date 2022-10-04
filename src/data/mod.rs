377d9c2a6dc1f0b5c048b0db1f9c1e45851c9194
Pierce Thompson <insprill@gmail.com>
2022-10-03 23:34:24 -0400

    Add cars to the app state

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,5 +1,6 @@
 use druid::{Data, Lens};
 use crate::Config;
+use crate::data::ccl_car::CclCar;
 
 pub mod config;
 pub mod ccl_car;
@@ -7,12 +8,15 @@ pub mod ccl_car;
 #[derive(Clone, Data, Lens)]
 pub struct AppState {
     pub config: Config,
+    #[data(ignore)]
+    pub cars: Vec<CclCar>,
 }
 
 impl AppState {
     pub fn from_config(config: Config) -> Self {
         Self {
             config,
+            cars: Vec::new()
         }
     }
 }
