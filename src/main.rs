4dd215fec2941c77f1cd24aa4888e7c36b258de2
Pierce Thompson <insprill@gmail.com>
2022-10-05 05:13:35 -0400

    Code style

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,35 @@
 use std::fs::File;
+
 use druid::AppLauncher;
-use log::{LevelFilter};
-use simplelog::{ColorChoice, CombinedLogger, TerminalMode, TermLogger, WriteLogger};
+use log::LevelFilter;
+use simplelog::{ColorChoice, CombinedLogger, TermLogger, TerminalMode, WriteLogger};
 
-use crate::data::AppState;
 use crate::data::config::Config;
+use crate::data::AppState;
 
-mod ui;
 mod data;
+mod ui;
 
 fn main() {
-    CombinedLogger::init(
-        vec![
-            TermLogger::new(LevelFilter::Info, simplelog::Config::default(), TerminalMode::Mixed, ColorChoice::Auto),
-            WriteLogger::new(LevelFilter::Info, simplelog::Config::default(), File::create(Config::config_dir().expect("Failed to find config dir").join("latest.log")).unwrap())
-        ]
-    ).unwrap();
+    CombinedLogger::init(vec![
+        TermLogger::new(
+            LevelFilter::Info,
+            simplelog::Config::default(),
+            TerminalMode::Mixed,
+            ColorChoice::Auto,
+        ),
+        WriteLogger::new(
+            LevelFilter::Info,
+            simplelog::Config::default(),
+            File::create(
+                Config::config_dir()
+                    .expect("Failed to find config dir")
+                    .join("latest.log"),
+            )
+            .unwrap(),
+        ),
+    ])
+    .unwrap();
 
     let config = Config::load().unwrap_or_default();
     let state = AppState::from_config(config);
