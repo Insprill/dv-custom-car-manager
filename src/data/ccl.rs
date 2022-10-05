40fde4549ccce6e0d6f8e264fdb63b5c2871c7f2
Pierce Thompson <insprill@gmail.com>
2022-10-05 05:17:06 -0400

    Minor install from archive cleanup

--- src/data/ccl.rs
+++ src/data/ccl.rs
@@ -1,10 +1,12 @@
 use std::fs::{create_dir_all, File};
-use std::{fs, io};
 use std::io::BufReader;
 use std::path::{Path, PathBuf};
+use std::{fs, io};
 
+use log::{info, warn};
 use serde::{Deserialize, Serialize};
 use zip::ZipArchive;
+
 use crate::{AppState, Config};
 
 pub const CONFIG_NAME: &str = "car.json";
@@ -32,7 +34,8 @@ pub struct CarConfig {
 
 impl CarConfig {
     fn new(directory: &Path) -> Self {
-        let file = File::open(directory.join(CONFIG_NAME)).expect("Failed to find car configuration");
+        let file =
+            File::open(directory.join(CONFIG_NAME)).expect("Failed to find car configuration");
         serde_json::from_reader(file).expect("Failed to read car configuration")
     }
 }
@@ -43,7 +46,13 @@ pub fn cars_path(config: &Config) -> PathBuf {
 
 pub fn dir_contains_car(path: &PathBuf) -> bool {
     let mut dir = fs::read_dir(path).expect("Failed to read dir");
-    dir.any(|f| f.unwrap().file_name().to_string_lossy().to_string().eq(CONFIG_NAME))
+    dir.any(|f| {
+        f.unwrap()
+            .file_name()
+            .to_string_lossy()
+            .to_string()
+            .eq(CONFIG_NAME)
+    })
 }
 
 pub fn install_from_folder(path: PathBuf) {
@@ -56,10 +65,10 @@ pub fn install_from_archive(path: PathBuf, state: &AppState) {
     if !path.is_file() {
         panic!("TODO: not a file")
     }
-    let archive_file = File::open(path).unwrap();
+    let archive_file = File::open(path).expect("Failed to open archive file");
     let reader = BufReader::new(archive_file);
 
-    let mut archive = ZipArchive::new(reader).unwrap();
+    let mut archive = ZipArchive::new(reader).expect("Failed to open archive");
 
     if !archive.file_names().any(|name| name.ends_with(CONFIG_NAME)) {
         panic!("TODO: failed to find car")
@@ -68,31 +77,39 @@ pub fn install_from_archive(path: PathBuf, state: &AppState) {
     let cars_path = cars_path(&state.config);
 
     for i in 0..archive.len() {
-        let mut file = archive.by_index(i).unwrap();
+        let mut file = archive.by_index(i).expect("Failed to get archive file");
         let file_path = match file.enclosed_name() {
             Some(path) => cars_path.join(path),
             None => {
-                println!("Suspicious path {}", file.name());
+                warn!(
+                    "Not extracting file with suspicious path \"{}\"",
+                    file.name()
+                );
                 continue;
             }
         };
 
-        if file.name().ends_with('/') {
-            create_dir_all(&file_path).unwrap();
-        } else {
-            println!(
-                "File {} extracted to \"{}\" ({} bytes)",
-                i,
-                file_path.display(),
-                file.size()
-            );
-            if let Some(p) = file_path.parent() {
-                if !p.exists() {
-                    create_dir_all(&p).unwrap();
-                }
-            }
-            let mut outfile = File::create(&file_path).unwrap();
-            io::copy(&mut file, &mut outfile).unwrap();
+        if !file.is_file() {
+            continue;
         }
+
+        if let Some(p) = file_path.parent() {
+            if !p.exists() {
+                create_dir_all(&p).expect("Failed to create directory");
+                info!(
+                    "Created directory \"{}\"",
+                    file_path.file_name().unwrap().to_string_lossy().to_string()
+                );
+            }
+        }
+
+        let mut outfile = File::create(&file_path).expect("Failed to open file");
+        io::copy(&mut file, &mut outfile).expect("Failed to write file");
+        info!(
+            "Extracted \"{}\" to \"{}\" ({} bytes)",
+            file.name(),
+            file_path.display(),
+            file.size()
+        );
     }
 }
