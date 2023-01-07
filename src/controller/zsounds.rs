3a03119335db9cd7a7243b7501fcbdd44a165015
Pierce Thompson <insprill@gmail.com>
2023-01-07 01:21:26 -0500

    Temporary disable ZSounds audio playback Doesn't build on GitHub actions, doesn't work any. Will figure out later.

--- src/controller/zsounds.rs
+++ src/controller/zsounds.rs
@@ -1,9 +1,9 @@
 use druid::{widget::Controller, Env, Event, EventCtx, Widget};
 use log::error;
-use rodio::OutputStream;
-use std::path::Path;
-use std::thread;
-use std::{error::Error, fs::File};
+// use rodio::OutputStream;
+// use std::path::Path;
+// use std::thread;
+// use std::{error::Error, fs::File};
 
 use crate::{cmd, data::AppState, mods::Installable};
 
@@ -43,30 +43,30 @@ where
                     .install_from_folder(&file_info.path, &state.config);
             }
             Event::Command(cmd) if cmd.is(cmd::ZSOUNDS_PLAY_SOUND) => {
-                let sound = cmd.get_unchecked(cmd::ZSOUNDS_PLAY_SOUND);
-                match play_sound(&sound.path) {
-                    Ok(_) => {}
-                    Err(err) => {
-                        error!(
-                            "Failed to play sound {} ({:?}): {}",
-                            sound.filename, sound.path, err
-                        );
-                        //todo: alert
-                    }
-                }
-                todo!("Play sounds properly, update button when playing to allow the sound to be stopped");
+                // let sound = cmd.get_unchecked(cmd::ZSOUNDS_PLAY_SOUND);
+                // match play_sound(&sound.path) {
+                //     Ok(_) => {}
+                //     Err(err) => {
+                //         error!(
+                //             "Failed to play sound {} ({:?}): {}",
+                //             sound.filename, sound.path, err
+                //         );
+                //         //todo: alert
+                //     }
+                // }
+                todo!();
             }
             _ => child.event(ctx, event, state, env),
         }
     }
 }
 
-fn play_sound(path: &Path) -> Result<(), Box<dyn Error>> {
-    let (_stream, stream_handle) = OutputStream::try_default()?;
-    let sink = stream_handle.play_once(File::open(path)?)?;
-    sink.set_volume(0.5);
-    thread::spawn(move || {
-        sink.sleep_until_end();
-    });
-    Ok(())
-}
+// fn play_sound(path: &Path) -> Result<(), Box<dyn Error>> {
+//     let (_stream, stream_handle) = OutputStream::try_default()?;
+//     let sink = stream_handle.play_once(File::open(path)?)?;
+//     sink.set_volume(0.5);
+//     thread::spawn(move || {
+//         sink.sleep_until_end();
+//     });
+//     Ok(())
+// }
